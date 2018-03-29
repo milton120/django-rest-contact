a60ad03ffc4d22f57aaaa5cc368741033baeed50
milton120 <miltonmarma120@gmail.com>
2018-03-30 02:45:17 +0600

    validation is added in ContactSerializer

--- contact/api/serializers.py
+++ contact/api/serializers.py
@@ -7,6 +7,31 @@ class ContactSerializer(serializers.ModelSerializer):
         model = Contact
         fields = ('name', 'contact_number')
 
+    # validation for duplicate name
+    def validate_name(self, value):
+        name_matched = Contact.objects.filter(name__iexact=value)
+
+        if self.instance:
+            name_matched = name_matched.exclude(pk=self.instance.pk)  # exclude own name from the list of matched names
+
+        if name_matched.exists():
+            raise serializers.ValidationError("Name already exists")
+
+        return value
+
+    # validation for duplicate number
+    def validate_contact_number(self, value):
+        number_matched = Contact.objects.filter(contact_number__iexact=value)
+
+        if self.instance:
+            number_matched = number_matched.exclude(pk=self.instance.pk)
+
+        if number_matched.exists():
+            raise serializers.ValidationError("Number already exists")
+
+        return value
+
+
 
 
 
